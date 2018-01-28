069afd500407416422c3b0a18ff2456ed7d22e97
kimshuye <suntisuk.go@gmail.com>
2018-01-28 18:13:39 +0700

    add install package angularfire2 , firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule , Routes } from '@angular/router';
 
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseService } from './services/firebase.service';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductsComponent } from './components/products/products.component';
+import { environment } from '../environments/environment';
 
 const appRoutes: Routes = [
   {path:'',component:HomeComponent},
@@ -27,6 +32,8 @@ const appRoutes: Routes = [
     FormsModule,
     HttpModule,
     RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase ,AngularFireList } from 'angularfire2/database';
 
 @Injectable()
 export class FirebaseService {
